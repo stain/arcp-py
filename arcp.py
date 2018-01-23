756a4fb77108bddb78927859a879fe4b2c3f3868
Stian Soiland-Reyes <stain@apache.org>
2018-01-23 20:25:00 +0000

    arcp
    Python library for
    https://tools.ietf.org/id/draft-soilandreyes-arcp-00.txt

---
+++ arcp.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from uuid import uuid4, uuid5, UUID, NAMESPACE_URL
+import urllib.parse as urlparse
+
+SCHEME="arcp"
+
+def _register_scheme(scheme=SCHEME):
+    """Ensure app scheme works with urllib.parse.urljoin and friends"""    
+    for u in (urlparse.uses_relative, urlparse.uses_netloc, urlparse.uses_fragment):
+        if not scheme in u:
+            u.append(scheme)
+_register_scheme()
+
+def arcp_uuid(uuid, path="/", query=None, fragment=None):
+    if not isinstance(uuid, UUID):
+        # ensure valid UUID
+        uuid = UUID(uuid)
+
+    # TODO: Ensure valid path?    
+    path = path or ""
+    authority = "uuid," + uuid
+    s = (SCHEME, authority, path, query, fragment)
+    return urlparse.urlunsplit(s)
+
+def arcp_random(path="/", query=None, fragment=None, uuid=None):
+    if uuid is None:
+        uuid = uuid4()
+    elif not isinstance(uuid, UUID):
+        # ensure valid UUID
+        uuid = UUID(uuid)
+    if not uuid.version == 4:
+        raise Exception("UUID is not v4" % uuid)
+    return app_uuid(uuid, path=path, query=query, fragment=fragment)
+
+def arcp_location(location, path="/", query=None, fragment=None, namespace=NAMESPACE_URL):
+    # TODO: Ensure location is valid url?
+    uuid = uuid5(namespace, location)
+    return app_uuid(uuid, path=path, fragment=fragment)
+    
+def _reg_name_regex():
+    # unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
+    unreserved = r"[A-Za-z0-9-._~]"
+
+    # pct-encoded = "%" HEXDIG HEXDIG
+    pct_encoded = r"%[0-9A-Fa-f][0-9A-Fa-f]"
+    
+    # "!" / "$" / "&" / "'" / "(" / ")"
+    #  / "*" / "+" / "," / ";" / "="
+    sub_delims = r"[!$&'()*+,;=]"
+
+    # reg-name    = *( unreserved / pct-encoded / sub-delims )    
+    reg_name = r"(" + unreserved + r"|" + pct_encoded + sub_delims + r")*"
+    return re.compile(reg_name)
+
+_REG_NAME = _reg_name_regex()
+
+def arcp_name(name, path="/", query=None, fragment=None):
+    if not _REG_NAME.fullmatch(name):
+        raise Exception("Invalid name: %s" % name)
+    authority = "name," + name
+    s = (SCHEME, authority, path, query, fragment)
+    return urlparse.urlunsplit(s)
+
+def arcp_hash():
+    pass
