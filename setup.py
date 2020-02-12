25e5d44f4a6c4a87cc27249a8d6ff2f0093e5d91
Stian Soiland-Reyes <stain@apache.org>
2020-02-12 12:04:17 +0000

    release arcp 0.2.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
   name = 'arcp',
   packages = find_packages(exclude=['contrib', 'docs', 'tests']), # Required
-  version = '0.2.1.dev0',
+  version = '0.2.1',
   description = 'arcp (Archive and Package) URI parser and generator',
   long_description=long_description,
   author = 'Stian Soiland-Reyes',
